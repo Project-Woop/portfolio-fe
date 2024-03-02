8829658b7b0982814a2556e8d3c414f08afd5630
vincenzolomuscio77.vl <vincenzolomuscio77.vl@gmail.com>
2024-03-02 11:43:37 +0100

    [🎬] Project init

---
+++ tailwind.config.js
@@ -0,0 +1,16 @@
+/** @type {import('tailwindcss').Config} */
+module.exports = {
+  content: [
+    "./src/**/*.{html,ts}",
+  ],
+  theme: {
+    extend: {
+      colors: {
+      },
+      width: {
+        "160": "40rem"
+      }
+    },
+  },
+  plugins: [],
+};
