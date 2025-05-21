8783b794db350fc85cabde42bebdbe23e7debb34
vincenzolomuscio <vincenzo@projectwoop.it>
2025-05-21 12:29:23 +0200

    [🛠] Fix WCAG link list compliance

--- src/app/components/footer/footer.component.ts
+++ src/app/components/footer/footer.component.ts
@@ -15,20 +15,28 @@ import {RouterLink} from "@angular/router";
               <img src="assets/images/svg/logo.svg" class="logo pointer-events-none svg-primary dark:svg-primary-dark" alt="Woop logo">
               <div class="hidden text-xl sm:block pointer-events-none">woop</div>
           </div>
-          <div class="flex flex-row gap-4 justify-end items-center">
+          <ul class="flex flex-row gap-4 justify-end items-center">
+            <li>
               <a href="https://www.instagram.com/project.woop/" aria-label="project woop instagram profile">
                 <img src="assets/images/svg/instagram.svg" class="social svg-primary dark:svg-primary-dark" alt="instagram">
               </a>
+            </li>
+            <li>
               <a href="https://www.behance.net/vincenzolomuscio" aria-label="vincenzo lomuscio behance profile">
                   <img src="assets/images/svg/behance.svg" class="social svg-primary dark:svg-primary-dark" alt="behance">
               </a>
+            </li>
+            <li>
               <a href="https://www.linkedin.com/in/vincenzo-lomuscio-942815170/" aria-label="vincenzo lomuscio linkedin profile">
                 <img src="assets/images/svg/linkedin.svg" class="social svg-primary dark:svg-primary-dark" alt="linkedin">
               </a>
+            </li>
+            <li>
               <a href="mailto:vincenzo@projectwoop.it" aria-label="send email to project woop">
                   <img src="assets/images/svg/mail.svg" class="social svg-primary dark:svg-primary-dark" alt="email">
               </a>
-          </div>
+            </li>
+          </ul>
       </div>
 
   `,
